db601ae5b64711c9a624d3d1cb59125363342982
AlySyed-Bilal-Haider <bilalshahbscs@gmail.com>
2023-02-26 08:04:43 +0500

    mapbox with mapbiler grid

---
+++ src/calc.js
@@ -0,0 +1,117 @@
+import distance from "@turf/distance";
+import destination from "@turf/destination";
+export function getGrid(bbox, gridWidth, gridHeight, units) {
+  const earthCircumference = Math.ceil(
+    distance([0, 0], [180, 0], { units }) * 2
+  );
+  const maxColumns = Math.floor(earthCircumference / gridWidth);
+
+  const fullDistance = (from, to, options) => {
+    const dist = distance(from, to, options);
+    if (Math.abs(to[0] - from[0]) >= 180) {
+      return earthCircumference - dist;
+    }
+    return dist;
+  };
+
+  const features = [];
+  const west = bbox[0];
+  const south = bbox[1];
+  const east = bbox[2];
+  const north = bbox[3];
+
+  const deltaX =
+    (west < 0 ? -1 : 1) * fullDistance([0, 0], [west, 0], { units });
+  const deltaY =
+    (south < 0 ? -1 : 1) * fullDistance([0, 0], [0, south], { units });
+  const startDeltaX = Math.ceil(deltaX / gridWidth) * gridWidth;
+  const startDeltaY = Math.ceil(deltaY / gridHeight) * gridHeight;
+
+  const startPoint = [
+    destination([0, 0], startDeltaX, 90, { units }).geometry.coordinates[0],
+    destination([0, 0], startDeltaY, 0, { units }).geometry.coordinates[1],
+  ];
+
+  const width = fullDistance([west, 0], [east, 0], { units });
+  const height = fullDistance([0, south], [0, north], { units });
+  const columns = Math.min(Math.ceil(width / gridWidth), maxColumns);
+  const rows = Math.ceil(height / gridHeight);
+  let currentPoint;
+  currentPoint = startPoint;
+  for (let i = 0; i < columns; i++) {
+    const coordinates = [
+      [currentPoint[0], south],
+      [currentPoint[0], north],
+    ];
+    const feature = {
+      type: "Feature",
+      geometry: { type: "LineString", coordinates },
+      properties: {},
+    };
+    features.push(feature);
+
+    currentPoint = [
+      destination([currentPoint[0], 0], gridWidth, 90, { units }).geometry
+        .coordinates[0],
+      currentPoint[1],
+    ];
+  }
+  currentPoint = startPoint;
+  for (let i = 0; i < rows; i++) {
+    const coordinates = [
+      [west, currentPoint[1]],
+      [east, currentPoint[1]],
+    ];
+    const feature = {
+      type: "Feature",
+      geometry: { type: "LineString", coordinates },
+      properties: {},
+    };
+    features.push(feature);
+
+    currentPoint = [
+      currentPoint[0],
+      destination([0, currentPoint[1]], gridHeight, 0, { units }).geometry
+        .coordinates[1],
+    ];
+  }
+  return features;
+}
+export function getGridCell(point, gridWidth, gridHeight, units) {
+  const earthCircumference = Math.ceil(
+    distance([0, 0], [180, 0], { units }) * 2
+  );
+  const fullDistance = (from, to, options) => {
+    const dist = distance(from, to, options);
+    if (Math.abs(to[0] - from[0]) >= 180) {
+      return earthCircumference - dist;
+    }
+    return dist;
+  };
+
+  const deltaX =
+    (point[0] < 0 ? -1 : 1) * fullDistance([0, 0], [point[0], 0], { units });
+  const deltaY =
+    (point[1] < 0 ? -1 : 1) * fullDistance([0, 0], [0, point[1]], { units });
+  const minDeltaX = Math.floor(deltaX / gridWidth) * gridWidth;
+  const minDeltaY = Math.floor(deltaY / gridHeight) * gridHeight;
+  const maxDeltaX = Math.ceil(deltaX / gridWidth) * gridWidth;
+  const maxDeltaY = Math.ceil(deltaY / gridHeight) * gridHeight;
+  const bbox = /** @type {GeoJSON.BBox} */ ([
+    destination([0, 0], minDeltaX, 90, { units }).geometry.coordinates[0],
+    destination([0, 0], minDeltaY, 0, { units }).geometry.coordinates[1],
+    destination([0, 0], maxDeltaX, 90, { units }).geometry.coordinates[0],
+    destination([0, 0], maxDeltaY, 0, { units }).geometry.coordinates[1],
+  ]);
+  const coordinates = [
+    [
+      [bbox[0], bbox[1]],
+      [bbox[2], bbox[1]],
+      [bbox[2], bbox[3]],
+      [bbox[0], bbox[3]],
+      [bbox[0], bbox[1]],
+    ],
+  ];
+  console.log("coordinates.....:", coordinates, "bbox......:", bbox);
+  return bbox;
+}
---
+++ src/grid.js
@@ -0,0 +1,112 @@
+import { getGrid, getGridCell } from "./calc";
+export const GRID_CLICK_EVENT = "grid.click";
+export function randomString() {
+  return Math.floor(Math.random() * 10e12).toString(36);
+}
+export class Grid {
+  constructor(config) {
+    this.id = `grid-${randomString()}`;
+    this.config = config;
+    this.updateBound = this.update.bind(this);
+    this.onMapClickBound = this.onMapClick.bind(this);
+    this.checker = false;
+    this.status = false;
+  }
+  onAdd(map) {
+    this.map = map;
+    this.map.on("load", this.updateBound);
+    this.map.on("move", this.updateBound);
+    if (this.checker) {
+      this.map.on("mousemove", this.onMapClickBound);
+    }
+    this.map.on("click", this.onMapClickBound);
+
+    if (this.map.loaded()) {
+      this.update();
+    }
+    return document.createElement("div");
+  }
+  onRemove() {
+    if (!this.map) {
+      return;
+    }
+    const source = this.map.getSource(this.id);
+    if (source) {
+      this.map.removeLayer(this.id);
+      this.map.removeSource(this.id);
+    }
+    this.map.off("load", this.updateBound);
+    this.map.off("move", this.updateBound);
+
+    this.map.off("click", this.onMapClickBound);
+    this.map = undefined;
+  }
+  update() {
+    if (!this.map) {
+      return;
+    }
+    let grid = [];
+    if (this.active) {
+      grid = getGrid(
+        this.bbox,
+        this.config.gridWidth,
+        this.config.gridHeight,
+        this.config.units
+      );
+    }
+    const source = this.map.getSource(this.id);
+    if (!source) {
+      this.map.addSource(this.id, {
+        type: "geojson",
+        data: { type: "FeatureCollection", features: grid },
+      });
+      this.map.addLayer({
+        id: this.id,
+        source: this.id,
+        type: "line",
+        paint: this.config.paint ?? {},
+      });
+    } else {
+      source.setData({ type: "FeatureCollection", features: grid });
+    }
+  }
+  get active() {
+    if (!this.map) {
+      return false;
+    }
+    const minZoom = this.config.minZoom ?? 0;
+    const maxZoom = this.config.maxZoom ?? 22;
+    const zoom = this.map.getZoom();
+    return minZoom <= zoom && zoom < maxZoom;
+  }
+  get bbox() {
+    if (!this.map) {
+      throw new Error("Invalid state");
+    }
+    const bounds = this.map.getBounds();
+
+    if (bounds.getEast() - bounds.getWest() >= 360) {
+      bounds.setNorthEast([bounds.getWest() + 360, bounds.getNorth()]);
+    }
+    const bbox = bounds.toArray().flat();
+    return bbox;
+  }
+  onMapClick(event) {
+    this.checker = true;
+    console.log("checker......>", this.checker);
+    if (!this.map || !this.active) {
+      return;
+    }
+    this.onAdd(this.map);
+    const point = event.lngLat.toArray();
+    const bbox = getGridCell(
+      point,
+      this.config.gridWidth,
+      this.config.gridWidth,
+      this.config.units
+    );
+    const event2 = { bbox };
+
+    this.map.fire(GRID_CLICK_EVENT, event2);
+  }
+}
